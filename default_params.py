82d40d090bf32a769d28ebae45924d5b9f31a682
Tom <tpepels@gmail.com>
2023-10-25 14:30:52 +0200

    new default

--- default_params.py
+++ default_params.py
@@ -1,7 +1,7 @@
 DEFAULT_SETTINGS = {
     ("ninarow", "mcts"): {
         # C-value optimized for 9x9 5-in-a-row without improvements
-        "ai_params": {"c": 0.8, "imm_alpha": 0.2},
+        "ai_params": {"c": 0.8, "imm_alpha": 0.2, "dyn_early_term_cutoff": 0.2},
         "eval_params": {"m_power": 2, "m_centre": 3, "a": 200},
     },
     ("breakthrough", "mcts"): {
