071ddcbdea528a7548fd4c0c55b9a0815124d2e3
Tom <tpepels@gmail.com>
2023-11-21 19:43:40 +0100

    lorenz

--- games/breakthrough.py
+++ games/breakthrough.py
@@ -466,7 +466,7 @@ class BreakthroughGameState(GameState):
         "a": 7,
     }
 
-    default_params = array.array("d", [1, 1, 1, 1, 1, 1, 5.0, 60.0])
+    default_params = array.array("d", [1, 0, 0, 0, 0, 0, 0, 60.0])
 
     @cython.cfunc
     @cython.exceptval(-9999999, check=False)
