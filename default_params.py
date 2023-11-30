ac28db416641e3a053993fa0dd759036b7297f3d
Tom <tpepels@gmail.com>
2023-11-30 15:12:12 +0100

    final tests

--- default_params.py
+++ default_params.py
@@ -1,21 +1,23 @@
 DEFAULT_SETTINGS = {
     ("ninarow", "mcts"): {
-        # C_ab_imm: 0.4
-        # C_imm: 0.4
-        # C_vanilla: 0.5
-        # C_ab: 0.4
+        # C_ab_imm: 0.1 (imm: 0.6)
+        # C_imm: 0.1 (imm: 0.75)
+        # C_vanilla: 0.3
+        # C_ab: 0.3
         "ai_params": {
-            "c": 0.2,
-            "imm_alpha": 0.8,
+            "c": 0.1,
+            "imm_alpha": 0.75,
             "dyn_early_term_cutoff": 0.2,
         },
         "eval_params": {"m_power": 2, "m_centre": 3, "a": 400},
     },
     ("breakthrough", "mcts"): {
-        # imm value with alpha/beta: 0.6
-        # imm value without alpha/beta: 0.8
+        # c_ab_imm: 0.45 (imm 0.3)
+        # c_imm: 0.4 (imm 0.3)
+        # c_vanilla: 0.8
+        # c_ab: 0.55
         "ai_params": {
-            "c": 0.25,
+            "c": 0.4,
             "imm_alpha": 0.3,
             "early_term_turns": 10,
             # "pb_weight": 0.3,
@@ -35,13 +37,13 @@ DEFAULT_SETTINGS = {
         },
     },
     ("amazons", "mcts"): {
-        # C_ab_imm - 0.5
-        # C_imm - 0.25
-        # C_vanilla - 0.6
-        # C_ab - 0.5
+        # C_ab_imm: 0.1 (imm: 0.85)
+        # C_imm: 0.1 (imm: 0.9)
+        # C_vanilla: 0.5
+        # C_ab: 0.4
         "ai_params": {
-            "c": 0.25,
-            "imm_alpha": 0.85,
+            "c": 0.1,
+            "imm_alpha": 0.9,
             "early_term_turns": 10,
             "early_term_cutoff": 0.1,
             # "e_g_subset": 10,
