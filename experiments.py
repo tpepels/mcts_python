2c519262e451a5f00e514f311a8ac622bafb5136
Tom <tpepels@gmail.com>
2024-03-10 18:03:47 +0000

    Update experiment timeout value

--- experiments.py
+++ experiments.py
@@ -66,7 +66,7 @@ def run_single_experiment(
 experiments_to_cancel = {}
 
 
-def start_experiments_from_json(json_file_path, n_procs=8, count_only=False, agg_loc=None, timeout=20 * 60):
+def start_experiments_from_json(json_file_path, n_procs=8, count_only=False, agg_loc=None, timeout=25 * 60):
     with open(json_file_path) as json_file:
         data = json.load(json_file)
 
@@ -124,7 +124,7 @@ def start_experiments_from_json(json_file_path, n_procs=8, count_only=False, agg
             except CancelledError:
                 print("A task was cancelled and did not complete.")
             except TimeoutError as e:
-                print(f"Experiment timed out: {e}", sys.stderr)
+                print(f"Experiment timed out: {e} after {timeout=}", sys.stderr)
             except ProcessExpired as e:
                 print(f"Experiment process expired: {e}", sys.stderr)
             except Exception as e:
