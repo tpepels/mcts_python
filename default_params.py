de8edc0f6c080290e1fdc992dab6f1e283b9537d
Tom <tpepels@gmail.com>
2024-03-16 07:36:11 +0000

    Update default parameters and game settings for amazons

--- default_params.py
+++ default_params.py
@@ -39,13 +39,10 @@ DEFAULT_SETTINGS = {
         "eval_params": {},
     },
     ("amazons", "mcts"): {
-        # C_ab_imm: 0.1 (imm: 0.85)
-        # C_imm: 0.1 (imm: 0.9)
-        # C_vanilla: 0.5
-        # C_ab: 0.4
+        # These are tuned for 6x6 amazons (see below for 8x8 amazons results)
         "ai_params": {
-            "c": 0.1,
-            "imm_alpha": 0.8,
+            "c": 0.05,
+            "imm_alpha": 0.9,
             "early_term_turns": 20,
             "early_term_cutoff": 0.3,
             # "e_g_subset": 10,
@@ -58,3 +55,13 @@ DEFAULT_SETTINGS = {
         "eval_params": {"a": 300},
     },
 }
+
+# +---------------------------------+----------------------+------------+-----------------------------------------+------------+------------+-----------+
+# |               Exp.              |        Param1        | Wins (AI1) |                  Param2                 | Wins (AI2) | ± 95% C.I. | No. Games |
+# +---------------------------------+----------------------+------------+-----------------------------------------+------------+------------+-----------+
+# | amazons8_mcts_mcts_0315_2258_20 |    imm_alpha:0.8     |   38.42    |     ab_p1:2, ab_p2:2, imm_alpha:0.6     |   61.58    |   ±7.17    |    177    |
+# | amazons8_mcts_mcts_0315_2258_21 |    imm_alpha:0.8     |   42.53    |     ab_p1:2, ab_p2:2, imm_alpha:0.7     |   57.47    |   ±7.35    |    174    |
+# | amazons8_mcts_mcts_0315_2258_22 |                      |   44.77    |             ab_p1:2, ab_p2:2            |   55.23    |   ±7.43    |    172    |
+# |  amazons8_mcts_mcts_0315_2258_1 | c:0.1, imm_alpha:0.8 |   45.09    |          c:0.05, imm_alpha:0.7          |   54.91    |   ±7.41    |    173    |
+# | amazons8_mcts_mcts_0315_2258_19 | c:0.1, imm_alpha:0.8 |   45.40    | ab_p1:2, ab_p2:2, c:0.05, imm_alpha:0.9 |   54.60    |   ±7.40    |    174    |
+# |  amazons8_mcts_mcts_0315_2258_2 |        c:0.1         |   46.15    |                  c:0.05                 |   53.85    |   ±7.52    |    169    |
