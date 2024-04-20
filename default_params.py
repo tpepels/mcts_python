21aafc73e523a56644fdf45ce9ea9bdb868ecc5d
Tom <tpepels@gmail.com>
2024-04-20 13:00:26 +0200

    Update early termination parameters in default_params.py

--- default_params.py
+++ default_params.py
@@ -35,8 +35,8 @@ DEFAULT_SETTINGS = {
         "ai_params": {
             "c": 0.4,
             "imm_alpha": 0.3,
-            "early_term_turns": 10,
-            "early_term_cutoff": 0.2,
+            "early_term_turns": 16,
+            "early_term_cutoff": 0.1,
         },
         "eval_params": {},
     },
